00ea3087ad317cd119fff0924a601578253498db
ZorioDEV <143969174+ZorioDEV@users.noreply.github.com>
2025-08-24 15:50:42 -0400

    Lesson 54
    - Learned basic of React
    - Created styling
    - Created app component
    - Created clickers
    - Saved values with useMemo
    - Used useRef hooks
    - Created people component

---
+++ Lessons/54/54-first-react-application/src/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+const root = createRoot(document.querySelector('#root'))
+
+const toto = 'there'
+
+root.render(
+    <App clickersCount={ 3 }>
+        <h1>My First React App</h1>
+        <h2>And a fancy subtitle</h2>
+    </App>
+)
